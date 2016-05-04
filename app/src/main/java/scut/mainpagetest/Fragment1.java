fd4121bfdc40661963d10c658c23a0eef935354d
totond <yanzhikai_yjk@qq.com>
2016-05-04 18:30:44 +0800

    Initial commit

---
+++ app/src/main/java/scut/mainpagetest/Fragment1.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package scut.mainpagetest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Fragment1 extends Fragment implements MyItemClickListener {
+	private RecyclerView Rv;
+	private ArrayList<HashMap<String,Object>> listItem;
+	private MyAdapter myAdapter;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view=inflater.inflate(R.layout.mainpage_fragment_item1, null);
+		Rv = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+		initData();
+		initView();
+		return view;
+	}
+
+	public void initData(){
+		listItem = new ArrayList<HashMap<String, Object>>();/*在数组中存放数据*/
+		for (int i = 1; i < 101; i++) {
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("ItemTitle", "第" + i + "行");
+			map.put("ItemText", "这是第" + i + "行");
+			map.put("ItemImage",R.drawable.objects);
+			listItem.add(map);
+		}
+	}
+	public void initView(){
+		LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+
+
+		myAdapter = new MyAdapter(getActivity().getApplicationContext(),listItem);
+		myAdapter.setOnItemClickListener(this);
+		Rv.setLayoutManager(layoutManager);
+		Rv.setHasFixedSize(true);
+		Rv.setAdapter(myAdapter);
+
+	}
+
+	@Override
+	public void onItemClick(View view, int postion) {
+		System.out.println("点击了第"+postion+"行");
+		Toast.makeText(getActivity().getApplicationContext(), (String)listItem.get(postion).get("ItemText"), Toast.LENGTH_SHORT).show();
+	}
+
+}
